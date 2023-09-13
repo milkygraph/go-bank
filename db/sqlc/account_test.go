863709be39c00da52c576a562dd665ce47dc60ea
milkygraph <kenanbanda2004@gmail.com>
2023-09-13 20:24:42 +0400

    Add RESTfull http api using gin for accounts table

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -25,7 +25,6 @@ func createRandomAccount(t *testing.T) Account {
 	require.Equal(t, arg.Balance, account.Balance)
 	require.Equal(t, arg.Currency, account.Currency)
 
-	require.NotZero(t, account.ID)
 	require.NotZero(t, account.CreatedAt)
 
 	return account
