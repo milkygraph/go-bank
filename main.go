5ae1f97b65fb398809816919f247bf96480896fd
milkygraph <kenanbanda2004@gmail.com>
2023-09-14 00:09:58 +0400

    Add config file using viper
    Now we can read environment variables from the system and config file specified app.env file using viper library.

--- main.go
+++ main.go
@@ -7,16 +7,16 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/milkygraph/simplebank/api"
 	db "github.com/milkygraph/simplebank/db/sqlc"
-)
-
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:kurero17@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "127.0.0.1:8080"
+	"github.com/milkygraph/simplebank/util"
 )
 
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config: ", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
 	}
@@ -24,7 +24,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server: ", err)
 	}
