5846ab99fe480d50c421a275b6af4ec288037a28
milkygraph <kenanbanda2004@gmail.com>
2023-09-15 23:21:42 +0400

    Add custom validation function for currency

--- api/server.go
+++ api/server.go
@@ -2,6 +2,8 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 	db "github.com/milkygraph/simplebank/db/sqlc"
 )
 
@@ -15,6 +17,10 @@ func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 	gin.SetMode(gin.ReleaseMode)
+
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
 
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
